9ab5909b635bef57cbe680471a5bb5d04a01d776
Cameron Grieder <cjgrieder@gmail.com>
2022-01-03 07:34:54 -0700

    cleaned up code for readability

--- outlier.js
+++ outlier.js
@@ -4,12 +4,11 @@ const findOut = (arr)=>{
 let o = []
 let e = []
 for (i=0;i<arr.length;i++){
-if (arr[i]%2==0){
-e.push(arr[i])} 
-else if (arr[i]%2!==0){
-o.push(arr[i])}
-}
- 
+    if (arr[i]%2==0){
+    e.push(arr[i])} 
+    else if (arr[i]%2!==0){
+    o.push(arr[i])}
+} 
     if(o.length > e.length){
         return e.toString()
     } else {return o.toString()}
