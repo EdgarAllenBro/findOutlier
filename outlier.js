23c519c6f74a4158d3fac76c8031588c3c9ed0d1
Cameron Grieder <cjgrieder@gmail.com>
2022-01-03 08:08:54 -0700

    cleaned up code

--- outlier.js
+++ outlier.js
@@ -1,13 +1,12 @@
-const array = [1,3,5,7,2,9,11,13]
+const array = [12,32,52,72,22,9,112,132]
 
 const findOut = (arr)=>{
-let o = []
-let e = []
+    let o = []
+    let e = []
 for (i=0;i<arr.length;i++){
     if (arr[i]%2==0){
     e.push(arr[i])} 
-    else if (arr[i]%2!==0){
-    o.push(arr[i])}
+    else{o.push(arr[i])}
 } 
     if(o.length > e.length){
         return e.toString()
